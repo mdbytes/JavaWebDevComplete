21afa11acd8061639eb7f659bb23b23fc69437a8
martindwyer <martin@mdbytes.com>
2024-01-14 12:12:48 -0600

    Hibernate mappings - Advanced

--- section-16-hibernate-advanced/03-one-to-many/src/main/java/com/mdbytes/crud/demo/repository/InstructorDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mdbytes.crud.demo.repository;
-
-import com.mdbytes.crud.demo.entity.InstructorDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstructorDetailRepository extends JpaRepository<InstructorDetail,Integer> {
-}
